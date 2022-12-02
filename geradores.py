dc6c80f228b2897ffda39b574f5f45592c87f713
Felipe-76 <felipe.leitao14@gmail.com>
2022-12-02 20:17:16 -0300

    GUI

--- geradores.py
+++ geradores.py
@@ -2,8 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+
 class Gerador:
-    def __init__(self, r_a=0,r_adj=0,r_adjlim=30,r_f=0,w=0,Rload=0,IL=0,Vf=0):
+    def __init__(self, r_a=0.18,r_adj=0,r_adjlim=30,r_f=24,w=1800,Rload=0,IL=0,Vf=0):
         # if r_a <0 or r_adj<0 or r_adjlim<0 : raise ValueError('invalido')
         self.r_a= r_a
         self.r_adj=r_adj
@@ -25,32 +26,26 @@ class Gerador:
 
 
 class Excind(Gerador):
-    def __init__(self,r_a=None,r_adj=None,r_adjlim=None,r_f=None,w=None,Rload=None,IL=None,Vf=None,Vb=None,i_f=None, Vt=None):
+    def __init__(self,r_a=1,r_adj=1,r_adjlim=None,r_f=1,w=1800,Rload=None,IL=None,Vf=1,Vb=None,i_f=None, Vt=None):
         super().__init__(r_a,r_adj,r_adjlim,r_f,w,Rload,IL,Vf)
         self.Vb=Vb
+        self.r_f = self.r_f + self.r_adj
         self.i_f=self.Vf/self.r_f
     
 
     def simular(self):
+        
         def curvamag(corrente):
             ea_values=[0,8,16,24,32,40,48,56,64,71.9000000000000,79.5000000000000,86.7000000000000,93.1000000000000,98.7000000000000,103.500000000000,107.460000000000,110.660000000000,113.460000000000,115.860000000000,117.960000000000,120,122,123.900000000000,125.700000000000,127.400000000000,128.980000000000,130.420000000000,131.740000000000,132.950000000000,134.150000000000,135.400000000000,136.650000000000,138,139.250000000000,140.500000000000,141.800000000000]
             if_values=[0,0.2000,    0.4000,    0.6000,    0.8000,    1.0000,    1.2000,    1.4000,    1.6000,    1.8000,    2.0000,    2.2000,    2.4000,    2.6000,    2.8000,    3.0000,    3.2000,    3.4000,    3.6000,    3.8000,    4.0000,    4.2000,    4.4000,    4.6000,    4.8000,    5.0000,    5.2000,    5.4000,    5.6000,    5.8000,    6.0000,    6.2000,    6.4000,    6.6000,    6.8000,    7.0000]
     
-            return np.interp(corrente, if_values, ea_values) 
+            return np.interp(corrente, if_values, ea_values)*self.w/1800
+
         EA=curvamag(self.i_f)
-        IA=np.linspace(0,40,300)
-        VT= EA-IA*self.r_a
-    
-    
-        fig, ax = plt.subplots()
-        ax.plot( IA, VT) 
-        fig.savefig("grafico.jpeg")
-        plt.xlabel('Corrente (A)')
-        plt.ylabel(' (V)')
-        plt.title('Simulação Gerador Excitação Independente')
-        plt.grid()
-    
-        return VT
+        IL=np.linspace(0,40,300)
+        VT= EA-IL*self.r_a
+
+        return IL, VT
  
 
 class Serie(Gerador):
@@ -63,7 +58,7 @@ class Serie(Gerador):
         return None
     
 class Shunt(Gerador):
-    def __init__(self,r_a=None,r_adj=None,r_adjlim=None,r_f=None,w=None,Rload=None,IL=None,Vf=None,Vb=None,i_f=None, Vt=None):
+    def __init__(self,r_a=None,r_adj=None,r_adjlim=None,r_f=None,w=1800,Rload=None,IL=None,Vf=None,Vb=None,i_f=None, Vt=None):
         super().__init__(r_a,r_adj,r_adjlim,r_f,w,Rload,IL,Vf)
         self.Vb=Vb
         self.i_f=i_f
@@ -88,7 +83,7 @@ class Shunt(Gerador):
         i_f = np.arange(0,6+0.02,0.02); # Field current (A)
         n = 1800; # Generator speed (r/min)
         # Calculate Ea versus If
-        Ea = np.interp(i_f,if_values,ea_values);
+        Ea = np.interp(i_f,if_values,ea_values) * self.w/n_0;
         
         # Calculate Vt versus If
         Vt = (r_f + r_adj) * i_f;
@@ -113,17 +108,17 @@ class Shunt(Gerador):
         
         self.resultados= f"Ea =   {Ea[ii]}  V \n Vt = {Vt[ii]} V \n If = {i_f[ii]} A"
         
-        figshunt, axshunt = plt.subplots()
-        axshunt.plot( i_f, Ea, 'b-') 
-        axshunt.plot( i_f, Vt, 'r.')
-        axshunt.legend(['Curva de  Magnetização', ' Tensão Terminal'])
-        plt.xlabel('Corrente (A)')
-        plt.ylabel(' (V)')
-        plt.title('Simulação Gerador Shunt')
-        plt.grid()
+       # figshunt, axshunt = plt.subplots()
+        #axshunt.plot( i_f, Ea, 'b-') 
+        #axshunt.plot( i_f, Vt, 'r.')
+        #axshunt.legend(['Curva de  Magnetização', ' Tensão Terminal'])
+       # plt.xlabel('Corrente (A)')
+       # plt.ylabel(' (V)')
+        #plt.title('Simulação Gerador Shunt')
+        #plt.grid()
         
-        figshunt.savefig("graficoshunt.jpeg")
-
+        #figshunt.savefig("graficoshunt.jpeg")
+        return i_f, Vt, Ea
 
     def __str__(self):
          return self.resultados
--- menu.py
+++ menu.py
@@ -2,11 +2,14 @@ import tkinter as tk
 from tkinter import RIGHT, Button, ttk
 import numpy as np
 from PIL import Image, ImageTk
-import matplotlib.pyplot as plt
 from mudar_tab import mudar_tab
 from geradores import *
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
+NavigationToolbar2Tk)
 
-gerador=Gerador()
+
+gerador = Gerador(r_a=0.19, r_f = 24, w = 1800, r_adj = 10)
 
 # root window
 root = tk.Tk()
@@ -32,20 +35,17 @@ notebook.pack(pady=0, expand=False)
 frame1 = ttk.Frame(notebook, width=800 , height=600)
 frame2 = ttk.Frame(notebook, width=800 , height=600)
 frame3 = ttk.Frame(notebook, width=800 , height=600)
-frame4 = ttk.Frame(notebook, width=800 , height=600)
 
 
 frame1.pack(fill='both', expand=True)  # Menu
 frame2.pack(fill='both', expand=True)  # Gerador Excitação independente
 frame3.pack(fill='both', expand=True)  # Gerador Shunt
-frame4.pack(fill='both', expand=True)  # Gerador Série
 
 # add frames to notebook
 
 notebook.add(frame1, text='Menu')
 notebook.add(frame2, text='Gerador de excitação independente')
 notebook.add(frame3, text='Gerador Shunt')
-notebook.add(frame4, text='Gerador Série')
 
 # add buttons to frames
 
@@ -74,9 +74,6 @@ label_RF = tk.Label(variaveis,text='Rf:').grid(row=2, column=0)
 caixa_RF = tk.Entry(variaveis, width=7)
 caixa_RF.grid(row=2, column=1)
 
-label_Vb = tk.Label(variaveis,text='Vb:').grid(row=3, column=0)
-caixa_Vb = tk.Entry(variaveis, width=7)
-caixa_Vb.grid(row=3, column=1)
 
 label_w = tk.Label(variaveis,text='w:').grid(row=4, column=0)
 caixa_w = tk.Entry(variaveis, width=7)
@@ -86,74 +83,52 @@ label_Rload = tk.Label(variaveis,text='Rload:').grid(row=5, column=0)
 caixa_Rload = tk.Entry(variaveis, width=7)
 caixa_Rload.grid(row=5, column=1)
 
-label_IL = tk.Label(variaveis,text='IL:').grid(row=6, column=0)
-caixa_IL = tk.Entry(variaveis, width=7)
-caixa_IL.grid(row=6, column=1)
 
 
 def armazenar():
-    Gerador.r_a=caixa_RA.get()
-    Gerador.r_adj=caixa_Raj.get()
-    Gerador.r_f=caixa_RF.get()
-    Gerador.Vf=caixa_Vb.get()
-    Gerador.w=caixa_w.get()
-    Gerador.Rload=caixa_Rload.get()
-    Gerador.IL=caixa_IL.get()
+    gerador.r_a=float(caixa_RA.get())
+    gerador.r_adj=float(caixa_Raj.get())
+    gerador.r_f=float(caixa_RF.get())
+    gerador.w=float(caixa_w.get())
+    gerador.Rload=float(caixa_Rload.get())
 
-    global valores_menu, valores_exc_ind, valores_shunt, valores_serie
+    global valores_menu, valores_exc_ind, valores_shunt
     valores_menu.grid_forget()
     valores_exc_ind.grid_forget()
     valores_shunt.grid_forget()
-    valores_serie.grid_forget()
 
     valores_menu = tk.LabelFrame(frame1, padx=50, pady=10, border=0)
     valores_exc_ind = tk.LabelFrame(frame2, padx=50, pady=10, border=0) 
     valores_shunt = tk.LabelFrame(frame3, padx=50, pady=10, border=0) 
-    valores_serie = tk.LabelFrame(frame4, padx=50, pady=10, border=0)
     valores_menu.grid(row=1, column=1,padx=100)
     valores_exc_ind.grid(row=0, column=0)
     valores_shunt.grid(row=0, column=0)
-    valores_serie.grid(row=0, column=0)
 
     tk.Label(valores_menu, text=f"RA = {gerador.r_a}").grid()
     tk.Label(valores_menu, text=f"Raj = {gerador.r_adj}").grid()
     tk.Label(valores_menu, text=f"RF = {gerador.r_f}").grid()
-    tk.Label(valores_menu, text=f"Vf = {gerador.Vf}").grid()
     tk.Label(valores_menu, text=f"w = {gerador.w}").grid()
     tk.Label(valores_menu, text=f"Rload = {gerador.Rload}").grid()
-    tk.Label(valores_menu, text=f"IL = {gerador.IL}").grid()
 
     tk.Label(valores_shunt, text=f"RA = {gerador.r_a}").grid()
     tk.Label(valores_shunt, text=f"Raj = {gerador.r_adj}").grid()
     tk.Label(valores_shunt, text=f"RF = {gerador.r_f}").grid()
-    tk.Label(valores_shunt, text=f"Vf = {gerador.Vf}").grid()
     tk.Label(valores_shunt, text=f"w = {gerador.w}").grid()
     tk.Label(valores_shunt, text=f"Rload = {gerador.Rload}").grid()
-    tk.Label(valores_shunt, text=f"IL = {gerador.IL}").grid()
-
-    tk.Label(valores_serie, text=f"RA = {gerador.r_a}").grid()
-    tk.Label(valores_serie, text=f"Raj = {gerador.r_adj}").grid()
-    tk.Label(valores_serie, text=f"RF = {gerador.r_f}").grid()
-    tk.Label(valores_serie, text=f"Vf = {gerador.Vf}").grid()
-    tk.Label(valores_serie, text=f"w = {gerador.w}").grid()
-    tk.Label(valores_serie, text=f"Rload = {gerador.Rload}").grid()
-    tk.Label(valores_serie, text=f"IL = {gerador.IL}").grid()
 
     tk.Label(valores_exc_ind, text=f"RA = {gerador.r_a}").grid()
     tk.Label(valores_exc_ind, text=f"Raj = {gerador.r_adj}").grid()
     tk.Label(valores_exc_ind, text=f"RF = {gerador.r_f}").grid()
-    tk.Label(valores_exc_ind, text=f"Vf = {gerador.Vf}").grid()
     tk.Label(valores_exc_ind, text=f"w = {gerador.w}").grid()
     tk.Label(valores_exc_ind, text=f"Rload = {gerador.Rload}").grid()
-    tk.Label(valores_exc_ind, text=f"IL = {gerador.IL}").grid()
 
 
 
 
 
 ## Botões
-button_armazenar = tk.Button(frame1, text='Armazenar valores', command=armazenar)
-button_armazenar.grid(row = 2, column = 0)
+button_armazenar = tk.Button(frame1, text='Armazenar valores', command=armazenar, pady=10)
+button_armazenar.grid(pady=10, row = 2, column = 0)
 
 ## Frame para armazenar alguns botões
 botoes_menu = tk.LabelFrame(frame1, padx=50, pady=10, border=0)
@@ -164,14 +139,11 @@ botao_exc_ind = tk.Button(botoes_menu, text='Gerador excitação independente', 
 botao_exc_ind.grid(row=0, pady=10, sticky="WE")
 botao_shunt = tk.Button(botoes_menu, text='Gerador Shunt', command=lambda:mudar_tab(notebook, frame3))
 botao_shunt.grid(row=1, pady=10, sticky="WE")
-botao_serie = tk.Button(botoes_menu, text='Gerador Série', command=lambda:mudar_tab(notebook, frame4))
-botao_serie.grid(row=2, pady=10, sticky="WE")
 
 ## Frame para armazenar os valores atuais das variáveis (Esse frame será usado nas outras abas)
 valores_menu = tk.LabelFrame(frame1, padx=50, pady=10, border=0)
 valores_exc_ind = tk.LabelFrame(frame2, padx=50, pady=10, border=0) 
 valores_shunt = tk.LabelFrame(frame3, padx=50, pady=10, border=0) 
-valores_serie = tk.LabelFrame(frame4, padx=50, pady=10, border=0) 
 valores_menu.grid(row=1, column=1, padx=100)
 
 ### Texto dento do frame com os valores atuais
@@ -179,53 +151,96 @@ valores_menu.grid(row=1, column=1, padx=100)
 tk.Label(valores_menu, text=f"RA = {gerador.r_a}").grid()
 tk.Label(valores_menu, text=f"Raj = {gerador.r_adj}").grid()
 tk.Label(valores_menu, text=f"RF = {gerador.r_f}").grid()
-tk.Label(valores_menu, text=f"Vf = {gerador.Vf}").grid()
 tk.Label(valores_menu, text=f"w = {gerador.w}").grid()
 tk.Label(valores_menu, text=f"Rload = {gerador.Rload}").grid()
-tk.Label(valores_menu, text=f"IL = {gerador.IL}").grid()
 
 tk.Label(valores_shunt, text=f"RA = {gerador.r_a}").grid()
 tk.Label(valores_shunt, text=f"Raj = {gerador.r_adj}").grid()
 tk.Label(valores_shunt, text=f"RF = {gerador.r_f}").grid()
-tk.Label(valores_shunt, text=f"Vf = {gerador.Vf}").grid()
 tk.Label(valores_shunt, text=f"w = {gerador.w}").grid()
 tk.Label(valores_shunt, text=f"Rload = {gerador.Rload}").grid()
-tk.Label(valores_shunt, text=f"IL = {gerador.IL}").grid()
-
-tk.Label(valores_serie, text=f"RA = {gerador.r_a}").grid()
-tk.Label(valores_serie, text=f"Raj = {gerador.r_adj}").grid()
-tk.Label(valores_serie, text=f"RF = {gerador.r_f}").grid()
-tk.Label(valores_serie, text=f"Vf = {gerador.Vf}").grid()
-tk.Label(valores_serie, text=f"w = {gerador.w}").grid()
-tk.Label(valores_serie, text=f"Rload = {gerador.Rload}").grid()
-tk.Label(valores_serie, text=f"IL = {gerador.IL}").grid()
 
 tk.Label(valores_exc_ind, text=f"RA = {gerador.r_a}").grid()
 tk.Label(valores_exc_ind, text=f"Raj = {gerador.r_adj}").grid()
 tk.Label(valores_exc_ind, text=f"RF = {gerador.r_f}").grid()
-tk.Label(valores_exc_ind, text=f"Vf = {gerador.Vf}").grid()
 tk.Label(valores_exc_ind, text=f"w = {gerador.w}").grid()
 tk.Label(valores_exc_ind, text=f"Rload = {gerador.Rload}").grid()
-tk.Label(valores_exc_ind, text=f"IL = {gerador.IL}").grid()
+
+def simular_exc_ind(gerador):
+    r_a = gerador.r_a
+    r_adj = gerador.r_adj
+    r_f = gerador.r_f
+    w = gerador.w
+    Rload = gerador.Rload
+    Vf = float(escala_Vf.get())
+
+    gerador_temp = Excind(r_a=r_a, r_adj=r_adj,r_adjlim=30,r_f=r_f, w = w, Rload = Rload, Vf = Vf)
+    IL, VT = gerador_temp.simular()
+
+    # Frame para colocar o grafico
+    frame_grafico = tk.LabelFrame(frame2, padx=50, pady=50, border=0)
+    frame_grafico.grid(row=1,column=1, columnspan=3)
+
+    fig = Figure(figsize = (3.3, 3.3), dpi = 100)
+    
+    # Subplot
+    plot1 = fig.add_subplot(111)
+    plot1.set_ylim(0, max(VT) + 15)
+    plot1.set_title("Característica terminal (Vt x IL)")
+  
+    # Plotando o gráfico
+    plot1.plot(IL, VT, "r-")
+    plot1.grid()
+
+    # Canvas do Tkinter que contém a figura
+    canvas = FigureCanvasTkAgg(fig, master = frame_grafico)
+    canvas.draw()
+
+    # Colocando o Canvas na janela
+    canvas.get_tk_widget().pack()
+  
+    # Menu abaixo do gráfico
+    toolbar = NavigationToolbar2Tk(canvas,frame_grafico)
+    toolbar.update()
 
 
+def simular_shunt(gerador):
+    r_a = gerador.r_a
+    r_adj = gerador.r_adj
+    r_f = gerador.r_f
+    w = gerador.w
+    Rload = gerador.Rload
+    gerador_temp = Shunt(r_a=r_a, r_adj=r_adj,r_adjlim=30,r_f=r_f, w = w, Rload = Rload)
+    i_f, Vt, Ea = gerador_temp.Simularshunt()
 
+    # Frame para colocar o grafico
+    frame_grafico = tk.LabelFrame(frame3, padx=50, pady=50, border=0)
+    frame_grafico.grid(row=1,column=1, padx=10)
+    
+    fig = Figure(figsize = (3.3, 3.3), dpi = 100)
 
+    # Subplot
+    plot1 = fig.add_subplot(111)
+    plot1.set_ylim(0, max(Vt) + 15)
 
+    # Plotando o gráfico
+    plot1.plot(i_f, Vt, "r-")
+    plot1.plot(i_f, Ea, "b-")
+    plot1.grid()
+    plot1.legend(['Curva de  Magnetização', ' Tensão Terminal'])
+    plot1.set_title("(Ea, Vt x IL)")
 
+    # Canvas do Tkinter que contém a figura
+    canvas = FigureCanvasTkAgg(fig, master = frame_grafico)
+    canvas.draw()
 
-#Exc ind
-"""
-def simularexcind():
-    gerador.r_adj= escala_Raj.get()
-    print('simulando exc ind')
-    return
+    # Colocando o Canvas na janela
+    canvas.get_tk_widget().pack()
+  
+    # Menu abaixo do gráfico
+    toolbar = NavigationToolbar2Tk(canvas,frame_grafico)
+    toolbar.update()
 
-
-label_escalaRaj1=tk.Label(frame2,text='Selecione a Resistencia de Ajuste do Campo').grid(row=1, column=0)
-escala_Raj = tk.Scale(frame2, from_=0,to=gerador.r_adjlim)
-escala_Raj.grid(row=1, column=1)
-"""
 
 botao_menu_1 = tk.Button(frame2, text = "Menu",width=16, height=3,  command = lambda:mudar_tab(notebook, frame1))
 botao_menu_1.grid(row=0,column=4)
@@ -234,23 +249,19 @@ label_escalaRaj=tk.Label(frame2,width = 30, text='Selecione a Tensao do Campo').
 escala_Vf = tk.Scale(frame2, from_=0, to=200)
 escala_Vf.grid(row=0, column=2)
 
-button_simularexcind = tk.Button(frame2, width=16, height=3, text='Simular!')
+button_simularexcind = tk.Button(frame2, width=16, height=3, text='Simular!', command = lambda:simular_exc_ind(gerador))
 button_simularexcind.grid(row=0, column=3, padx=80)
 valores_exc_ind.grid(row=0, column=0)
 
 
 # Shunt
-botao_menu_2 = tk.Button(frame3, text = "Menu", command = lambda:mudar_tab(notebook, frame1))
-botao_menu_2.grid()
-valores_shunt.grid(row=2, column=2)
+valores_shunt.grid(row=0, column=0)
 
+button_simularexcind = tk.Button(frame3, width=16, height=3, text='Simular!', command = lambda:simular_shunt(gerador))
+button_simularexcind.grid(row=0, column=1, padx=200)
 
-#série
-botao_menu_3 = tk.Button(frame4, text = "Menu", command = lambda:mudar_tab(notebook, frame1))
-botao_menu_3.grid()
-valores_serie.grid(row=2, column=2)
-
-
+botao_menu_2 = tk.Button(frame3, width=16, height=3, text = "Menu", command = lambda:mudar_tab(notebook, frame1))
+botao_menu_2.grid(row=0, column=2)
 
 
 root.mainloop()
